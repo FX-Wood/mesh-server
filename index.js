efc2908021ee5d06ee0ae5b69674ac81838f0e11
FX Wood <fxwood2@gmail.com>
2023-06-25 03:30:02 -0700

    routes are working

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
+const dotenv = require('dotenv')
 const express = require('express')
+const { PrismaClient } = require('@prisma/client')
 const cors = require('cors')
 const app = express()
 const port = 3000
 
+dotenv.config()
+
+const { chooseNextMesh, updateReward } = require('./lib/rewardLoop.js')
+
+const prisma = new PrismaClient()
 app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/mesh', async (req, res) => {
+    try {
+        const nextMesh = await chooseNextMesh()
+    } catch {
+        console.error()
+    }
+    res.json(nextMesh)
+})
+
+app.post('/cluster', async (req, res) => {
+    const clusterId = body.clusterId
+    const liked = body.liked
+    try {
+        const update = await updateReward(clusterId, liked)
+    } catch(err) {
+        console.error(err)
+    }
 })
 
 app.listen(port, () => {
